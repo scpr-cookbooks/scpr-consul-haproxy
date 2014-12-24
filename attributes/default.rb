8c82478b2e869675089c2c9c8fad1b42c76e1ba4
Eric Richardson <e@ericrichardson.com>
2014-12-24 14:07:23 -0500

    scpr-consul-haproxy 0.1.11: Add timeout and max connection attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,9 @@
 default.scpr_consul_haproxy.admin_interface = "eth0"
 default.scpr_consul_haproxy.config_key      = "haproxy/scpr"
+default.scpr_consul_haproxy.max_sessions    = 10000
+default.scpr_consul_haproxy.timeout_client  = "5m"
+default.scpr_consul_haproxy.timeout_connect = "10s"
+default.scpr_consul_haproxy.timeout_server  = "30s"
 
 #----------
 
