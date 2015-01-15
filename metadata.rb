c031a50a4c67f23eee0fe17793b821fe3efa22d2
Eric Richardson <e@ericrichardson.com>
2015-01-15 17:19:11 -0500

    scpr-consul-haproxy 0.1.15: streammachine—fix source in port

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'erichardson@scpr.org'
 license          'all_rights'
 description      'Installs/Configures scpr-consul-haproxy'
 long_description 'Installs/Configures scpr-consul-haproxy'
-version          '0.1.14'
+version          '0.1.15'
 
 depends 'haproxy'
 depends 'scpr-consul'
